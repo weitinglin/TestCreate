e99e754fca7342889e4d45865bf362c1bb5f4f5a
weitinglin <weitinglin66@gmail.com>
2017-05-29 17:32:18 +0800

    add create_phenodata description

--- R/create_phenodata.R
+++ R/create_phenodata.R
@@ -1,6 +1,16 @@
 # Basic function
 
-# load the raw cel data from path, add the experiment design and output phenodata data.frame
+
+#' Create the Phenodata for affy input
+#'
+#' @description  This function will create the phenodata which used in loading cel files
+#'
+#' @param experiment.set vector, store the experiment set in vector
+#' @param data.path str, the input raw cell directory path
+#' @return this function will return a tbl_df data , if the filename not exist in current directory, show error message
+#' @examples create_phenodata(experiment.set=c("control_1","control_2","case_1","case_2"), data.path='./cel_data_dir')
+
+
 create_phenodata <- function(experiment.set, data.path){
     set<- list.files (data.path, pattern=".CEL.gz")
     phenodata.set   <- matrix ( rep ( set, 2) , ncol = 2 )
